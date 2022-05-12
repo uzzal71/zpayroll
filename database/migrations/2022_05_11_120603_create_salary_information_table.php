4b7ac84af65a0f1ebab48ae6568e2de4f6e4f047
uzzal71 <uzzalroy.acm@gmail.com>
2022-05-12 09:49:07 +0600

    Bank and Salary Information Migration file added

--- database/migrations/2022_05_11_120603_create_salary_information_table.php
+++ database/migrations/2022_05_11_120603_create_salary_information_table.php
@@ -15,7 +15,17 @@ class CreateSalaryInformationTable extends Migration
     {
         Schema::create('salary_information', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('employee_id');
+            $table->double('gross_salary', 10, 2)->default(0.00);
+            $table->double('basic_salary', 10, 2)->default(0.00);
+            $table->double('hourly_rate', 10, 2)->default(0.00);
+            $table->double('house_rent', 10, 2)->default(0.00);
+            $table->double('medical_allowance', 10, 0)->default(0.00);
+            $table->double('transport_allowance', 10, 2)->default(0.00);
+            $table->double('food_allowance', 10, 2)->default(0.00);
             $table->timestamps();
+
+            $table->foreign('employee_id')->references('id')->on('employees');
         });
     }
 
--- database/migrations/2022_05_11_120624_create_bank_information_table.php
+++ database/migrations/2022_05_11_120624_create_bank_information_table.php
@@ -15,7 +15,16 @@ class CreateBankInformationTable extends Migration
     {
         Schema::create('bank_information', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('employee_id');
+            $table->string('account_name')->nullable();
+            $table->string('account_number')->nullable();
+            $table->string('bank_name')->nullable();
+            $table->string('bank_branch_name')->nullable();
+            $table->text('bank_address')->nullable();
+            $table->string('swift_code')->nullable();
             $table->timestamps();
+
+            $table->foreign('employee_id')->references('id')->on('employees');
         });
     }
 
