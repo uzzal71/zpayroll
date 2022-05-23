2ad3beeb5f2872a88cdfd315ced686f582fa06ad
uzzal71 <uzzalroy.acm@gmail.com>
2022-05-23 12:23:51 +0600

    Employee Payslip Function Change

--- app/Http/Controllers/EmployeeReportController.php
+++ app/Http/Controllers/EmployeeReportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Attendance;
 use App\Models\AttendanceSummary;
+use App\Models\SalarySheet;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -66,38 +67,25 @@ class EmployeeReportController extends Controller
         $year = date('Y');
         $employee_id = Auth::user()->id;
 
-        $attendances  = Attendance::where([
+        $employee_payslips  = SalarySheet::where([
                 'employee_id' => $employee_id,
-                'attendance_month' => $month,
-                'attendance_year' => $year
-            ])->orderBy('id', 'asc');
-
-        $attendance_summary = AttendanceSummary::where([
-                'employee_id' => $employee_id,
-                'attendance_month' => $month,
-                'attendance_year' => $year
+                'salary_month' => $month,
+                'salary_year' => $year
             ])->orderBy('id', 'asc');
 
         if ($request->has('month')){
             $month = $request->month;
             $year = $request->year;
 
-            $attendances  = Attendance::where([
+            $employee_payslips  = SalarySheet::where([
                 'employee_id' => $employee_id,
-                'attendance_month' => $month,
-                'attendance_year' => $year
-            ])->orderBy('id', 'asc');
-
-            $attendance_summary = AttendanceSummary::where([
-                'employee_id' => $employee_id,
-                'attendance_month' => $month,
-                'attendance_year' => $year
+                'salary_month' => $month,
+                'salary_year' => $year
             ])->orderBy('id', 'asc');
         }
 
-        $attendances  = $attendances->get();
-        $attendance_summary  = $attendance_summary->first();
+        $employee_payslips  = $employee_payslips->get();
 
-        return view('employee_reports.employee_payslip', compact('attendances', 'attendance_summary', 'month', 'year'));
+        return view('employee_reports.employee_payslip', compact('employee_payslips', 'month', 'year'));
     }
 }
