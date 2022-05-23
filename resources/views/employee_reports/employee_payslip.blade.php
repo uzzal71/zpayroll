3550a66341c31a23812c4b599e0a535ccbed7c8e
uzzal71 <uzzalroy.acm@gmail.com>
2022-05-23 12:26:50 +0600

    employee payslip page design changed

--- resources/views/employee_reports/employee_payslip.blade.php
+++ resources/views/employee_reports/employee_payslip.blade.php
@@ -60,73 +60,157 @@
                 <!-- Search Form -->
             </div>
         </div>
-        @if(!empty($attendances))
-        @if(!empty($attendance_summary))
+        @if(!empty($employee_payslips))
         <div class="card">
             <div class="card-body">
-                <h5>Attendance Summary</h5>
-                <table class="table table-bordered">
-                    <tr>
-                        <td>Total Days</td>
-                        <td>{{ $attendance_summary->number_of_days }}</td>
-                        <td>Weekend</td>
-                        <td>{{ $attendance_summary->weekend }}</td>
-                    </tr>
-                    <tr>
-                        <td>Holiday</td>
-                        <td>{{ $attendance_summary->holiday }}</td>
-                        <td>Need To Work</td>
-                        <td>{{ $attendance_summary->need_to_work }}</td>
-                    </tr>
-                    <tr>
-                        <td>Present</td>
-                        <td>{{ $attendance_summary->present }}</td>
-                        <td>Late</td>
-                        <td>{{ $attendance_summary->late }}</td>
-                    </tr>
-                    <tr>
-                        <td>Absent</td>
-                        <td>{{ $attendance_summary->absent }}</td>
-                        <td>Unpaid Leave</td>
-                        <td>{{ $attendance_summary->unpaid_leave }}</td>
-                    </tr>
-                    <tr>
-                        <td>Paid Leave</td>
-                        <td>{{ $attendance_summary->paid_leave }}</td>
-                        <td>Need To Pay</td>
-                        <td>{{ $attendance_summary->need_to_pay }}</td>
-                    </tr>
-                </table>
-                <h5>Attendance Time Sheet</h5>
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th>Date</th>
-                            <th>In-Time</th>
-                            <th>Out-Time</th>
-                            <th>Late-Time</th>
-                            <th>Over-Time</th>
-                            <th>Status</th>
-                            <th>Remarks</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($attendances as $key => $row)
-                        <tr>
-                            <td>{{ $row->attendance_date }}</td>
-                            <td>{{ $row->in_time }}</td>
-                            <td>{{ $row->out_time }}</td>
-                            <td>{{ $row->late_time }}</td>
-                            <td>{{ $row->over_time }}</td>
-                            <td>{{ $row->attendance_status }}</td>
-                            <td>{{ $row->remarks }}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                <div class="container mt-5 mb-5">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="text-center lh-1 mb-2">
+                                <h6 class="fw-bold">Payslip</h6> <span class="fw-normal">Payment slip for the month of June 2021</span>
+                            </div>
+                            <div class="d-flex justify-content-end"> <span>Working Branch:ROHINI</span> </div>
+                            <div class="row">
+                                <div class="col-md-10">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div> <span class="fw-bolder">EMP Code</span> <small class="ms-3">39124</small> </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div> <span class="fw-bolder">EMP Name</span> <small class="ms-3">Ashok</small> </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div> <span class="fw-bolder">PF No.</span> <small class="ms-3">101523065714</small> </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div> <span class="fw-bolder">NOD</span> <small class="ms-3">28</small> </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div> <span class="fw-bolder">ESI No.</span> <small class="ms-3"></small> </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div> <span class="fw-bolder">Mode of Pay</span> <small class="ms-3">SBI</small> </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div> <span class="fw-bolder">Designation</span> <small class="ms-3">Marketing Staff (MK)</small> </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div> <span class="fw-bolder">Ac No.</span> <small class="ms-3">*******0701</small> </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <table class="mt-4 table table-bordered">
+                                    <thead class="bg-dark text-white">
+                                        <tr>
+                                            <th scope="col">Earnings</th>
+                                            <th scope="col">Amount</th>
+                                            <th scope="col">Deductions</th>
+                                            <th scope="col">Amount</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">Basic</th>
+                                            <td>16250.00</td>
+                                            <td>PF</td>
+                                            <td>1800.00</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">DA</th>
+                                            <td>550.00</td>
+                                            <td>ESI</td>
+                                            <td>142.00</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">HRA</th>
+                                            <td>1650.00 </td>
+                                            <td>TDS</td>
+                                            <td>0.00</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">WA</th>
+                                            <td>120.00 </td>
+                                            <td>LOP</td>
+                                            <td>0.00</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">CA</th>
+                                            <td>0.00 </td>
+                                            <td>PT</td>
+                                            <td>0.00</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">CCA</th>
+                                            <td>0.00 </td>
+                                            <td>SPL. Deduction</td>
+                                            <td>500.00</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">MA</th>
+                                            <td>3000.00</td>
+                                            <td>EWF</td>
+                                            <td>0.00</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">Sales Incentive</th>
+                                            <td>0.00</td>
+                                            <td>CD</td>
+                                            <td>0.00</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">Leave Encashment</th>
+                                            <td>0.00</td>
+                                            <td colspan="2"></td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">Holiday Wages</th>
+                                            <td>500.00</td>
+                                            <td colspan="2"></td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">Special Allowance</th>
+                                            <td>100.00</td>
+                                            <td colspan="2"></td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">Bonus</th>
+                                            <td>1400.00</td>
+                                            <td colspan="2"></td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">Individual Incentive</th>
+                                            <td>2400.00</td>
+                                            <td colspan="2"></td>
+                                        </tr>
+                                        <tr class="border-top">
+                                            <th scope="row">Total Earning</th>
+                                            <td>25970.00</td>
+                                            <td>Total Deductions</td>
+                                            <td>2442.00</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4"> <br> <span class="fw-bold">Net Pay : 24528.00</span> </div>
+                                <div class="border col-md-8">
+                                    <div class="d-flex flex-column"> <span>In Words</span> <span>Twenty Five thousand nine hundred seventy only</span> </div>
+                                </div>
+                            </div>
+                            <div class="d-flex justify-content-end">
+                                <div class="d-flex flex-column mt-2"> <span class="fw-bolder">For Kalyan Jewellers</span> <span class="mt-4">Authorised Signatory</span> </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        @endif
         @endif
     </div>
 </div>
